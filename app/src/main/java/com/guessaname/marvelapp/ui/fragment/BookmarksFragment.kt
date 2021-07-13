ad9888ad50a074b6f3c41902721b99d57f2d16c8
lucadilorenzo <luca.dilorenzo98@gmail.com>
2021-07-13 17:50:33 +0200

    need to fix blank spaces on top and bottom of RV, comics related to character

--- app/src/main/java/com/guessaname/marvelapp/ui/fragment/BookmarksFragment.kt
+++ app/src/main/java/com/guessaname/marvelapp/ui/fragment/BookmarksFragment.kt
@@ -36,7 +36,6 @@ class BookmarksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_book_mark, container, false)
     }
